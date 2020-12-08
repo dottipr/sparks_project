800d8afa881e9d18d54e32c9dadfd9091df8814d
priscacheese <prisca.dotti@unifr.ch>
2020-12-08 16:43:13 +0100

    get sparks signals (1D and 2D) from preds instead of annotations

--- sparks/get_spark_signals.py
+++ sparks/get_spark_signals.py
@@ -1,13 +1,21 @@
 # 30.11.2020
 # Functions to extract the signal of the sparks present in the annotations
-# Run this script to plot a few samples
+# Run this script to plot a few samples (1D signal and 2D signal)
 
 import numpy as np
-import matplotlib.pyplot as plt
+import glob
 import os
 import imageio
-from scipy import ndimage
+import ntpath
 
+from scipy import spatial, optimize, ndimage
+from skimage import morphology
+
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import cm
+
+from correspondences import process_spark_prediction
 
 def create_circular_mask(h, w, center, radius):
     # h : image height
@@ -43,39 +51,38 @@ def create_signal_mask(t, h, w, start, stop, center, radius):
     return video_mask
 
 
-def get_spark_signal(video, sparks_labelled, idx, radius, context_duration,
+def get_spark_signal(video,
+                     sparks_labelled,
+                     center,
+                     radius,
+                     context_duration,
                      return_info = False):
-    # video :            the original video sample
-    # sparks_labelled :  mask containing the segmentation of the spark events
-    #                    (1 integer for every event)
-    # idx :              index of the selected event to plot
-    #                    (0,1,....,num_sparks-1)
-    # radius :           radius of the considered region around the center the
-    #                    spark for averaging
-    # context_duration : number of frames included in the analysis before and
-    #                    after the event
-    # returns signal average and list of corresponding frames
+    # video is the original video sample
+    # sparks_labelled is a mask containing the segmentation of the spark events (1 integer for every event)
+    # center [t y x] is the center of the selected event to plot
+    # radius is the radius of the considered region around the center the spark for averaging
+    # context_duration is the number of frames included in the analysis before and after the event
 
+    t,y,x = center
+    event_idx = sparks_labelled[t,y,x] # event_idx = 1,2,...,n_sparks
 
-    assert (idx < sparks_labelled.max()),(
-    f"given idx is too large, video contains only {sparks_labelled.max()} events")
+    assert event_idx != 0, (
+    "given center does not correspond to any event in the given labelled mask")
 
-    loc = ndimage.measurements.find_objects(sparks_labelled)[idx]
-    roi = sparks_array[loc].astype(bool)
+    loc = ndimage.measurements.find_objects(sparks_labelled)[event_idx-1]
 
-    # extract center (x_c,y_c) of the event
-    center = ndimage.measurements.center_of_mass(np.sum(roi, axis=0))
-    center = tuple(map(lambda x: isinstance(x, float) and round(x), center))#int
-    y,x = int(loc[1].start+center[0]), int(loc[2].start+center[1])
+    assert loc[0].start <= t and loc[0].stop > t, "something weird is wrong"
 
     # get mask representing sparks location (with radius and context)
-    start = max(0, loc[0].start - context_duration)
-    stop = min(video.shape[0], loc[0].stop + context_duration)
-    signal_mask = create_signal_mask(*sparks_array.shape, start, stop,
+    start = max(0,loc[0].start - context_duration)
+    stop = min(video.shape[0],loc[0].stop + context_duration)
+    signal_mask = create_signal_mask(*sparks_labelled.shape,
+                                     start, stop,
                                      (x,y), radius)
 
     frames = np.arange(start,stop)
-    signal = np.average(video_array[start:stop], axis=(1,2),
+    signal = np.average(video[start:stop],
+                        axis=(1,2),
                         weights=signal_mask[start:stop])
 
     if return_info:
@@ -84,53 +91,166 @@ def get_spark_signal(video, sparks_labelled, idx, radius, context_duration,
     return frames, signal
 
 
+def get_spark_2d_signal(video,
+                        slices,
+                        coords,
+                        spatial_context,
+                        sigma = 2,
+                        return_info = False):
+    # video : original video
+    # slices : slices in the 3 dimensions of a given spark (ROI)
+    # coords [t y x]: center of a given spark
+    # spatial_context : extend ROI corresponding to spark
+    # sigma : for gaussian filtering
+
+    # TODO: add assertion to check that coords are inside slices
+
+    t,y,x = coords
+    t_slice, y_slice, x_slice = slices
+
+    y_start = max(0, y_slice.start-spatial_context)
+    y_end = min(video.shape[1], y_slice.stop+spatial_context)
+
+    x_start = max(0, x_slice.start-spatial_context)
+    x_end = min(video.shape[2], x_slice.stop+spatial_context)
+
+    signal_2d = video[t, y_start:y_end, x_start:x_end]
+
+    # average over 3 frames
+    #signal_2d_avg = video_array[t-1:t+1, y_start:y_end, x_start:x_end]
+    #signal_2d_avg = np.average(signal_2d_avg, axis=0)
+
+    # smooth signal
+    signal_2d_gaussian = ndimage.gaussian_filter(signal_2d, 2) # Best
+
+    if return_info:
+        y_frames = np.arange(y_start, y_end)
+        x_frames = np.arange(x_start, x_end)
+
+        return t, y, x, y_frames, x_frames, signal_2d_gaussian
+
+    return signal_2d_gaussian
+
+
+
 if __name__ == "__main__":
 
     # Import .tif events file as numpy array
     data_path = os.path.join("..","data","annotation_masks")
-    sample_name = "130918_C_ET-1.tif"
-    events_name = os.path.join(data_path,"masks_test",sample_name)
-    video_name = os.path.join(data_path,"videos_test",sample_name)
+    sample_name = "140708_B_ET-1"
 
-    events_array = np.asarray(imageio.volread(events_name)).astype('int')
+    sparks_name = os.path.join("spark_preds_TEMP",sample_name+"_sparks.tif")
+    video_name = os.path.join(data_path,"videos_test",sample_name+".tif")
+
+    sparks_array = np.asarray(imageio.volread(sparks_name))
     video_array = np.asarray(imageio.volread(video_name)).astype('int')
 
+    # Remove first and last frames
+    half_overlap = 6 # TODO: get this from code
+    sparks_array[:half_overlap] = 0
+    sparks_array[-half_overlap:] = 0
 
-    # only analyse spark events, get rid of the rest
-    sparks_array = np.where(events_array==1,1,0)
+    # Get sparks centres from preds
+    min_radius = 3 # minimal 'radius' of a valid spark
+    t_detection = 0.95 # threshold on spark preds to extract events
+
+    coords_spark_centres = process_spark_prediction(sparks_array,
+                                                   t_detection=t_detection,
+                                                   min_radius=min_radius)
 
 
-    # separate events: assign an integer to every connected component
-    sparks_labelled, n_sparks = ndimage.measurements.label(sparks_array,
+    # Get sparks ROIs from preds from every spark centre
+    min_size = (2 * min_radius) ** sparks_array.ndim
+
+    spark_mask = sparks_array > t_detection
+    spark_mask = morphology.remove_small_objects(spark_mask,
+                                                 min_size=min_size)
+
+    # Separate events: assign an integer to every connected component
+    sparks_labelled, n_sparks = ndimage.measurements.label(spark_mask,
                                                            np.ones((3,3,3),
                                                            dtype=np.int))
+    # events labelled with values 1,2,...,n_sparks
 
-    # get a list with subarrays indices of sparks_labelled for every event
-    find_sparks = ndimage.measurements.find_objects(sparks_labelled)
+    # Compute single ROIs
+    sparks_rois = ndimage.measurements.find_objects(sparks_labelled)
+    # i-th element correspond to event with index i+1
 
-    # define radius around event center and context duration
+    # Assign a ROI (slices) to each centre
+    slices_spark_centres = []
+    for t,y,x in coords_spark_centres:
+        event_nb = sparks_labelled[t,y,x]
+        slices_spark_centres.append(sparks_rois[event_nb-1])
+
+
+    # Define radius around event center and context duration
     radius = 3
     context_duration = 30
 
-    # Plot few signal samples
+    # Get 1D signal along time and plot it for a few samples
     plt.rcParams.update({'font.size': 6})
 
     plt.figure(figsize=(20,10))
     plt.suptitle("Signal around some sample sparks", fontsize=10)
 
-    for idx in range(10):
-        frames, signal, (y,x), start, stop = get_spark_signal(video_array,
-                                                              sparks_labelled,
-                                                              idx,radius,
-                                                              context_duration,
-                                                              return_info=True)
+    for idx, centre in enumerate(coords_spark_centres[:10]):
+        frames,signal,(y,x),start,stop = get_spark_signal(video_array,
+                                                          sparks_labelled,
+                                                          centre,radius,
+                                                          context_duration,
+                                                          return_info=True)
 
         ax = plt.subplot(2,5,idx+1)
-        ax.set_title(f"Spark at position ({x},{y}) at frames {start}-{stop}")
-        ax.axvspan(start, stop, facecolor='green', alpha=0.3)
+        ax.set_title(f"Location ({x},{y}), frames {start}-{stop}")
+        ax.axvspan(start, stop, facecolor='green', alpha=0.25)
         plt.plot(frames,signal,color='darkgreen')
 
 
     #plt.tight_layout()
     plt.subplots_adjust(hspace=0.2, wspace=0.3)
+    plt.show()
+
+    # Get 2D signal around center of some sparks and plot them
+    spatial_context = 10
+
+    plt.figure(figsize=(20,10))
+
+    if n_sparks == 1:
+        plt.suptitle("2D smoothed signal of a sample spark", fontsize=10)
+        idx = 1
+
+        t,y,x,y_axis,x_axis,signal_2d = get_spark_2d_signal(video_array,
+                                                            slices_spark_centres[idx-1],
+                                                            coords_spark_centres[idx-1],
+                                                            spatial_context,
+                                                            sigma = 2,
+                                                            return_info = True)
+
+        x_axis, y_axis = np.meshgrid(x_axis,y_axis)
+
+        ax = plt.subplot(1,1,idx, projection='3d')
+        ax.set_title(f"Spark at location ({x},{y}) at frame {t}")
+        ax.plot_surface(y_axis, x_axis, signal_2d, cmap=cm.coolwarm, linewidth=0)#, antialiased=False)
+    else:
+        plt.suptitle("2D smoothed signals some sample sparks", fontsize=10)
+        plt.rcParams.update({'font.size': 8})
+
+        num_plots = min(n_sparks,10)
+        for idx in range(1,num_plots+1):
+            n_cols = num_plots//2 if num_plots%2 == 0 else (num_plots//2)+1
+            t,y,x,y_axis,x_axis,signal_2d = get_spark_2d_signal(video_array,
+                                                                slices_spark_centres[idx-1],
+                                                                coords_spark_centres[idx-1],
+                                                                spatial_context,
+                                                                sigma = 2,
+                                                                return_info = True)
+
+            x_axis, y_axis = np.meshgrid(x_axis,y_axis)
+
+            ax = plt.subplot(2,n_cols,idx, projection='3d')
+            ax.set_title(f"Location ({x},{y}), frame {t}")
+            ax.plot_surface(y_axis, x_axis, signal_2d, cmap=cm.coolwarm, linewidth=0)#, antialiased=False)
+
+    plt.tight_layout()
+    plt.subplots_adjust(hspace=0.1, wspace=0.1, top=0.9, left=0.05, right=0.95)
     plt.show()
